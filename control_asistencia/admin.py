d4cbaa955887de9f1276a1a0334f49fefc30a91e
Francisco Risso <frisso@topazevolution.com>
2023-05-12 00:55:57 -0300

    Implementar Modelos: Materia y Asistencia.

--- control_asistencia/admin.py
+++ control_asistencia/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import  Materia, Asistencia
 
-# Register your models here.
+
+admin.site.register(Materia)
+
+class AsistenciaAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+admin.site.register(Asistencia, AsistenciaAdmin)
--- control_asistencia/models.py
+++ control_asistencia/models.py
@@ -1,4 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, User
+import uuid
+
+class Materia(models.Model):
+    uid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    nombre = models.CharField(max_length=255)
+    icono = models.CharField(max_length=255)
+
+    docente = models.ForeignKey(User, on_delete=models.CASCADE, related_name='materiasDocente', limit_choices_to={'groups__name': 'Grupo_DOCENTES'})
+    estudiantes = models.ManyToManyField(User, related_name='materiasEstudiante',limit_choices_to={'groups__name': 'Grupo_ESTUDIANTES'})
+    
+    def __str__(self):
+        return self.nombre
+
+class Asistencia(models.Model):
+    uid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    fecha = models.DateField()
+    estudiante = models.ForeignKey(User, on_delete=models.CASCADE, related_name='asistenciasEstudiante')
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE, related_name='asistenciasMateria')
+
+    def __str__(self):
+        return self.fecha.strftime(format="%Y-%m-%d") + " - " + self.materia.nombre + " - " + self.estudiante.username
+
+'''
+
+class 
 
 class Asistencia(models.Model):
     nombre = models.CharField(max_length=255)
@@ -8,3 +34,17 @@ class Asistencia(models.Model):
     def __str__(self):
         return self.fecha.strftime("%d/%m/%y") + " - " + self.cedula + " - " + self.nombre
 
+
+
+
+from django.db import models
+
+class Docente(models.Model):
+    nombre = models.CharField(max_length=50)
+
+class Materia(models.Model):
+    nombre = models.CharField(max_length=50)
+    docente = models.ForeignKey(Docente, on_delete=models.CASCADE, related_name='materias')
+
+
+'''
