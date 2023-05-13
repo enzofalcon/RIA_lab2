3aecba3332238515d94632f3b9882782f35141b4
Francisco Risso <frisso@topazevolution.com>
2023-05-13 20:29:33 -0300

    Agregar lógica faltante en las vistas. Agregar modelo QR.

--- control_asistencia/views/docente_views.py
+++ control_asistencia/views/docente_views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 from ..utils.group_redirect import group_required
+from ..models import Materia, Asistencia, QR
 
 """
 /docente
@@ -16,7 +16,8 @@ Navega a marcar asistencia.
 @login_required
 @group_required('Grupo_DOCENTES')
 def index(request):
-    return render(request, "control_asistencia/docente/index.html")
+    materias = Materia.objects.filter(docente_id = request.user.id)
+    return render(request, "control_asistencia/docente/index.html", {"materias":materias})
 
 
 
@@ -28,7 +29,9 @@ Listar fechas en las cuales cierta materia tiene asistencias
 @login_required
 @group_required('Grupo_DOCENTES')
 def seleccionar_materia_fecha(request, id):
-    return render(request, "control_asistencia/docente/seleccionar_materia_fecha.html", {"id":id})
+    materia = Materia.objects.filter(uid = id).first()
+    fechas = Asistencia.objects.filter(materia_id = id).order_by("fecha").values_list('fecha', flat=True).distinct()
+    return render(request, "control_asistencia/docente/seleccionar_materia_fecha.html", {"materia":materia, "fechas": fechas})
 
 
 
@@ -40,7 +43,9 @@ Listas asistencias para cierta materia en cierta fecha
 @login_required
 @group_required('Grupo_DOCENTES')
 def asistencias_materia(request, id, fecha):
-    return render(request, "control_asistencia/docente/asistencias.html", {"id":id, "fecha":fecha})
+    materia = Materia.objects.filter(uid = id).first()
+    asistencias = Asistencia.objects.filter(materia_id = id, fecha=fecha)    
+    return render(request, "control_asistencia/docente/asistencias.html", {"materia":materia, "fecha":fecha, "asistencias":asistencias})
 
 
 
@@ -53,4 +58,7 @@ Generar QR para marcar asistencia en cierta materia en la fecha de hoy
 @login_required
 @group_required('Grupo_DOCENTES')
 def qr_asistencia(request, id):
-    return render(request, "control_asistencia/docente/qr_asistencia.html", {"id":id})
+    materia = Materia.objects.filter(uid=id).first()
+    qr = QR(fecha = timezone.now().strftime("%Y-%m-%d"), materia_id = id)
+    qr.save()
+    return render(request, "control_asistencia/docente/qr_asistencia.html", {"materia":materia, "qr":qr})
--- control_asistencia/views/estudiante_views.py
+++ control_asistencia/views/estudiante_views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from ..utils.group_redirect import group_required
-from ..models import Asistencia, Materia
+from ..models import Asistencia, Materia,  QR
 
 """
 /estudiante
@@ -42,4 +42,15 @@ Dado un link, introducir asistencia
 @login_required
 @group_required('Grupo_ESTUDIANTES')
 def marcar_asistencia(request, id):
-    return render(request, "control_asistencia/estudiante/marcar_asistencia.html", {"id":id})
+    qr = QR.objects.filter(uid = id).first()
+    materia = qr.materia
+    mensaje = None
+
+    #Marcar asistencia solo si ya no existe una asistencia para esta fecha, materia y estudiante.
+    if Asistencia.objects.filter(estudiante_id = request.user.id, materia_id = materia.uid, fecha = timezone.now().strftime("%Y-%m-%d")).exists():
+        mensaje = 'Ya has registrado tu asistencia'
+    else:
+        Asistencia(fecha = timezone.now().strftime("%Y-%m-%d"), estudiante_id = request.user.id, materia_id = materia.uid).save()
+        mensaje = 'Has registrado tu asistencia correctamente'
+
+    return render(request, "control_asistencia/estudiante/marcar_asistencia.html", {"materia":materia, "mensaje":mensaje})
